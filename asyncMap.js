e031d0f243a58d4b76de57fdf12a8bd9afcbded6
Leran Firer <leranf@gmail.com>
2016-06-02 11:41:14 -0700

    asyncMap

--- asyncMap.js
+++ asyncMap.js
@@ -1,32 +1,60 @@
-var runAsyncFunctions = function(tasks, callback) {
+/* Implement the function asyncMap:
+ *
+ * asyncMap has two parameters, an array of asynchronous functions (tasks) and a callback.
+ * Each of the tasks takes a separate callback and invokes that callback when complete.
+ *
+ * The callback passed to asyncMap is then performed on the results of the callbacks of the tasks.
+ *
+ * The order of these results should be the same as the order of the tasks.
+ * It is important to note that this is not the order in which the tasks return,
+ * but the order in which they are passed to asyncMap.
+ *
+ * Once all the callbacks of the tasks are returned, asyncMap should invoke the callback
+ * on the results array.
+ *
+ *
+ * Example:
+ *
+ * asyncMap([
+ *  function(cb){
+ *    setTimeout(function(){
+ *      cb('one');
+ *    }, 200);
+ *  },
+ *  function(cb){
+ *    setTimeout(function(){
+ *      cb('two');
+ *    }, 100);
+ *  }
+ * ],
+ *  function(results){
+ *    // the results array will equal ['one','two'] even though
+ *    // the second function had a shorter timeout.
+ *    console.log(results); // ['one', 'two']
+ * });
+ *
+ *
+ */
+
+var asyncMap = function(tasks, callback) {
   var results = [];
-  var counter = 0;
+  var count = 0;
 
   for (var i = 0; i < tasks.length; i++) {
     (function(i) {
-      tasks[i](function(val) {
-        console.log(val);
-        results[i] = val;
-        counter++;
-        if (counter === tasks.length) {
+      tasks[i](function(result) {
+        results[i] = result;
+        count++;
+        if (count === tasks.length) {
           callback(results);
         }
       });
     })(i);
   }
-}
 
-runAsyncFunctions([
-  function(cb){
-    setTimeout(function(){
-      cb('one');
-    }, 200);
-  },
-  function(cb){
-    setTimeout(function(){
-      cb('two');
-    }, 100);
-  }
-], function(results) {
-    console.log(results);
-});
+};
+
+
+
+
+
